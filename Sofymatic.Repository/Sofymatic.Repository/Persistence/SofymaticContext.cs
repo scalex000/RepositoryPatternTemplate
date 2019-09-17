378ea60f7f95aa83faf1e5e5fb48e3f82b7dde12
Saul Concepcion <scalex000@gmail.com>
2019-09-16 22:52:15 -0400

    Add SofymaticContext.cs to the project.

---
+++ Sofymatic.Repository/Sofymatic.Repository/Persistence/SofymaticContext.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Sofymatic.Repository.Core.Domain;
+using Sofymatic.Repository.Persistence.EntityConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sofymatic.Repository.Persistence
+{
+    public class SofymaticContext : DbContext
+    {
+        public SofymaticContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+        }
+
+    }
+}
