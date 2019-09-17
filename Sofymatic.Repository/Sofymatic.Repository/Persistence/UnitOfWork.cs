c4654f9598a8393c7052cb66c2c3277dd597ae39
Saul Concepcion <scalex000@gmail.com>
2019-09-16 22:50:27 -0400

    Add IUnitOfWork.cs and UnitOfWork.cs to project.

---
+++ Sofymatic.Repository/Sofymatic.Repository/Persistence/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Sofymatic.Repository.Core;
+using Sofymatic.Repository.Core.Repositories;
+using Sofymatic.Repository.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sofymatic.Repository.Persistence
+{
+    public class UnitOfWork : IDisposable, IUnitOfWork
+    {
+        private readonly SofymaticContext _context;
+
+        public UnitOfWork(SofymaticContext context)
+        {
+            _context = context;
+
+            //put here your repository 
+            Customers = new CustomerRepository(_context);
+        }
+
+        public ICustomerRepository Customers { get; private set; }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
