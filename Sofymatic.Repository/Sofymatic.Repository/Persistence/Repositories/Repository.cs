be9faead09bcbf49bde24a3f78ace5b973152ff9
Saul Concepcion <scalex000@gmail.com>
2019-09-16 21:51:05 -0400

    Add Repository.cs in the project.
    Add Repository.cs in the project and implement IRepository.

---
+++ Sofymatic.Repository/Sofymatic.Repository/Persistence/Repositories/Repository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sofymatic.Repository.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Sofymatic.Repository.Persistence.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private DbSet<TEntity> _entities;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _entities = context.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            _entities.Add(entity);
+        }
+
+        public void AddRange(TEntity entities)
+        {
+            _entities.AddRange(entities);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _entities.Where(predicate);
+        }
+
+        public TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _entities.SingleOrDefault(predicate);
+        }
+
+        public TEntity Get(int id)
+        {
+            return _entities.Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _entities.ToList();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _entities.Remove(entity)
+        }
+
+        public void RemoveRange(TEntity entities)
+        {
+            _entities.RemoveRange(entities);
+        }
+
+        public TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _entities.SingleOrDefault(predicate)
+        }
+    }
+}
