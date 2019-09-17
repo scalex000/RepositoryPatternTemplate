ae18a9bc13b5a4e44c887ec1a90febcd478dcf8f
Saul Concepcion <scalex000@gmail.com>
2019-09-16 22:51:31 -0400

    Add Customer class.

---
+++ Sofymatic.Repository/Sofymatic.Repository/Core/Domain/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sofymatic.Repository.Core.Domain
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
