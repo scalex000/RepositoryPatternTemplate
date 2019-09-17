0531748db1f131b60f8ebf5962a5dd1d170798f0
Saul Concepcion <scalex000@gmail.com>
2019-09-16 21:14:03 -0400

    Add IRepository.

---
+++ Sofymatic.Repository/Sofymatic.Repository/Core/Repositories/IRepository.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Repository its a colletion of object so, you don't return IEnumerable,
+ * DTO/Model, and do not perform updates.
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Sofymatic.Repository.Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity:  class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(int id);
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> precicate);
+        void Add(TEntity entity);
+        void AddRange(TEntity entities);
+        IEnumerable<TEntity> All(Expression<Func<TEntity, bool>> predicate);
+        void Remove(TEntity Entity);
+        void RemoveRange(TEntity entities);
+        TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predictate);
+    }
+}
