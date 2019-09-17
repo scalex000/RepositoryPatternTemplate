15ae3240edbde8e4674376f6dc91983a835e2520
Saul Concepcion <scalex000@gmail.com>
2019-09-16 22:51:09 -0400

    Add CustomerRepository.cs and ICustomerRepository.cs to the project.

---
+++ Sofymatic.Repository/Sofymatic.Repository/Persistence/Repositories/CustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sofymatic.Repository.Core.Domain;
+using Sofymatic.Repository.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Sofymatic.Repository.Persistence.Repositories
+{
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(DbContext context) : base(context)
+        {
+        }
+
+        public SofymaticContext SofymaticContext
+        {
+            get { return Context as SofymaticContext; }
+        }
+    }
+}
