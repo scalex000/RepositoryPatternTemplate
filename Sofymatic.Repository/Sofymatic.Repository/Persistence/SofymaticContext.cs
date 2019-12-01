82256816a78d3bbed9072f9b5dc6e6b3c4d85ff3
Saul Concepcion <scalex000@gmail.com>
2019-12-01 13:55:34 -0400

    Add OnConfiguring method.

--- Sofymatic.Repository/Sofymatic.Repository/Persistence/SofymaticContext.cs
+++ Sofymatic.Repository/Sofymatic.Repository/Persistence/SofymaticContext.cs
@@ -5,25 +5,30 @@ using Sofymatic.Repository.Persistence.EntityConfigurations;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.FileExtensions;
 
 namespace Sofymatic.Repository.Persistence
 {
     public class SofymaticContext : DbContext
     {
-        //public SofymaticContext(): base("name=SofymaticContext")
-        //{
-        //    this.Configuration.LazyLoadingEnabled = false;
-        //}
-        public SofymaticContext(DbContextOptions options) : base(options)
+        public SofymaticContext()
         {
 
         }
-
         public virtual DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\sqlexpress;Database=Sofymatic;Trusted_Connection=True;MultipleActiveREsultSets=true");
+            }
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
             modelBuilder.ApplyConfiguration(new CustomerConfiguration());
         }
     }
